0b3af71dcdcbd5e6418544123afc91d3dad51e91
rslibed <rslibed@gmail.com>
2018-01-11 17:03:15 -0800

    Completed add item with dummy data

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import { Route } from 'react-router-dom';
 import AddForm from'./add_form';
 import List from './list';
 
 const App = () => (
     <div className="container">
-        <List/>
-        <AddForm/>
+        <Route path="/" exact component={List}/>
+        <Route path="/add-item" component={AddForm}/>
     </div>
 );
 
--- src/components/list.js
+++ src/components/list.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class List extends Component {
     render () {
+        console.log('List props: ', this.props);
+        const listItems = this.props.list.map( (item, index) => {
+            return <li className="list-group-item" key={index}>{item.title}</li>
+        });
         return(
-            <div>
+            <div className="text-center">
                 <div className="row my-4 justify-content-end">
                     <div className="col-2">
                         <Link to="/add-item" className="btn btn-outline-primary">Add To Do Item</Link>
                     </div>
                 </div>
                 <h1>To Do List</h1>
+                <ul className="list-group">
+                    { listItems }
+                </ul>
             </div>
         )
     }
 }
 
-export default List;
+function mapStateToProps (state) {
+    return {
+        list: state.todo.list
+    }
+}
+
+export default connect(mapStateToProps)(List);
